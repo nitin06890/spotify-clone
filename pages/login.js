05ef0c7a4663b8b6b5dc1b9215d45c66b5ed1df0
nitin06890 <nitin06890@gmail.com>
2022-01-10 10:31:27 +0530

    updated login page

--- pages/login.js
+++ pages/login.js
@@ -1,55 +1,19 @@
-import { ChevronDoubleRightIcon } from '@heroicons/react/outline';
 import { getProviders, signIn } from 'next-auth/react';
 
 function Login({providers}) {
   return (
-    <div
-      className="text-center h-screen w-screen flex items-center justify-center relative"
-      //style={{ backgroundColor: 'whitesmoke' }}
-    >
-      <div className="bg-black w-screen h-40 flex items-center justify-center relative">
-        <img
-          className="w-16 md-20 lg:w-24 bg-white rounded-full mr-7 md:mr-12 "
-          src="https://i.imgur.com/fPuEa9V.png"
-          alt="spotify logo"
-        />
-        <h1
-          className="text-4xl lg:text-9xl md:text-7xl  uppercase font-bold"
-          style={{
-            color: '#1ed760',
-          }}
-        >
-          spotify
-        </h1>
-        <small className="absolute top-[6.5rem] right-[24rem] font-extrabold uppercase text-lg text-white hidden lg:block">
-          Clone
-        </small>
-
+      <div className="flex flex-col items-center bg-black justify-center min-h-screen w-full">
+        <img className="w-52 mb-5" src='https://i.imgur.com/fPuEa9V.png' alt="spotify logo"/>
         {Object.values(providers).map((provider) => (
           <div
-            key={provider.name}
-            className="absolute top-auto w-20 h-20 bg-transparent right-10 cursor-pointer"
-          >
-            <ChevronDoubleRightIcon
-              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-              className="w-20 h-20 animate-pulse"
-              style={{ color: '#1ed760' }}
-            />
+            key={provider.name}>
+            <button className="bg-[#1ed760] text-white p-5 rounded-full"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })} >
+                 Login with {provider.name}
+              </button>
           </div>
         ))}
-      </div>
-      <footer
-        className="absolute bottom-0 w-96 h-16 bg-gray-300 rounded-t-md"
-        style={{
-          textAlign: 'center',
-          display: 'flex',
-          marginTop: '1rem',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-        }}
-      >
-      </footer>
-    </div>
+      </div>   
   );
 }
 
